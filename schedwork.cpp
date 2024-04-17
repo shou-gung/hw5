64d413346140d3dab472e617e2abd8c439ccd11b
Samuel Rexrode <rexrode@usc.edu>
2024-04-17 11:32:38 +0000

    submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,25 +21,86 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool makeSchedule(
+  size_t n_row, 
+  size_t d_col, 
+  const AvailabilityMatrix& avail, const size_t maxShifts, 
+  DailySchedule& sched,
+  map<Worker_T, int>& freqCounter
+  );
 
 // Add your implementation of schedule() and other helper functions here
 
+// n - number of days to be worked
+// k - all possible workers
+// d - amount of workers needed per day
+// m - maximum shifts workers can do 
+
 bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
+    const AvailabilityMatrix& avail, // n x k availability matrix
+    const size_t dailyNeed, // d - amount of workers needed per day
+    const size_t maxShifts, // m - maximum shifts worker can do
+    DailySchedule& sched // n x d schedule that needs to be made
 )
 {
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
+    
     // Add your code below
+    if (maxShifts == 0) {
+      return false;
+    } else if (dailyNeed == 0) {
+      return true;
+    }
 
+    size_t n_days = avail.size();
 
+    for (size_t i = 0; i < n_days; i++) {
+      vector<Worker_T> day_sched(dailyNeed, INVALID_ID);
+      sched.push_back(day_sched);
+    }
 
-
+    map<Worker_T, int> freqCounter; 
+    
+    return makeSchedule(0, 0, avail, maxShifts, sched, freqCounter);
 }
 
+bool makeSchedule(
+  size_t n_row, 
+  size_t d_col, 
+  const AvailabilityMatrix& avail, const size_t maxShifts, 
+  DailySchedule& sched,
+  map<Worker_T, int>& freqCounter
+  ) {
+    
+    // base condition
+    if (n_row == avail.size()) {
+      return true;
+    }
+
+    if (d_col == sched[0].size()) {
+      return makeSchedule(n_row + 1, 0, avail, maxShifts, sched, freqCounter);
+    }
+
+    // recursive logic/condition
+    for (int work_id = 0; work_id < avail[n_row].size(); work_id++) { // iterating through k workers
+      // check if worker is available and hasn't worked more than maximum shifts
+      if (avail[n_row][work_id] && freqCounter[work_id] < maxShifts) {
+        sched[n_row][d_col] = work_id;
+        freqCounter[work_id]++;
+
+        // recursive call 
+        if (makeSchedule(n_row, d_col + 1, avail, maxShifts, sched, freqCounter)) {
+          return true;
+        }
+
+        // backtracking logic
+        sched[n_row][d_col] = INVALID_ID;
+        freqCounter[work_id]--;
+      }
+    }
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,84 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+bool validWord(const string& word, const set<string>& dict);
+
+void genTheWords(
+    string current, // current build/combination of the word
+    int index, // index for in
+    const string& in, const string& floating, const set<string>& dict, 
+    set<string>& valid_words, // set of valid words to find
+    map<char, int>& float_usage // map that tracks how many times a floating char has been used (0-1) 
+                                // 0 = not used
+                                // 1 = used
+);
 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
+    const std::string& in, /* - string of fixed, correct characters and `-`s to indicate the 
+                                length of string and locations that must be filled in */
+    const std::string& floating, // - Characters that must be used somewhere in the word
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> valid_words; // set of valid words that fit criteria
+    string init_w = in; // taking input of valid letters
+    map<char, int> float_usage; 
 
+    genTheWords("", 0, in, floating, dict, valid_words, float_usage);
+
+    return valid_words;
 }
 
 // Define any helper functions here
+
+bool validWord(const string& word, const set<string>& dict) {
+    return dict.find(word) != dict.end();
+}
+
+void genTheWords(
+    string current, 
+    int index, 
+    const string& in, const string& floating, const set<string>& dict, 
+    set<string>& valid_words,
+    map<char, int>& float_usage) 
+{
+        // base condition
+        if (index == in.length()) {
+          
+          // check if all floating chars are in current
+          for (size_t i = 0; i < floating.length(); i++) {
+            char ph = floating[i];
+            if (float_usage[ph] == 0) {
+              return;
+            }
+          }
+
+          if (validWord(current, dict)) {
+              valid_words.insert(current);
+          }
+
+          return;
+        }
+
+        char next_char = in[index];
+        if (next_char != '-') { // for already predetermined letters
+          genTheWords(current + next_char, index + 1, in, floating, dict, valid_words, float_usage);
+        } else {
+          // for loop handling float chars
+          for (size_t i = 0; i < floating.length(); i++) {
+            char ph = floating[i];
+            if (float_usage[ph] == 0) {
+              float_usage[ph]++;
+              genTheWords(current + ph, index + 1, in, floating, dict, valid_words, float_usage);
+              float_usage[ph]--;
+            }
+          }
+
+          // for loop handling general alphabet
+          for (char ph = 'a'; ph <= 'z'; ph++) {
+            genTheWords(current + ph, index + 1, in, floating, dict, valid_words, float_usage);
+          }
+        }
+}
